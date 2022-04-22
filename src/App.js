4413867d822ce2f9f01ac799558045db03adc9e4
Umang A <umanga.eng@gmail.com>
2022-04-21 20:05:55 -0400

    Feat : added Navbar component to app, navbar takes links prop to render different links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./routes/Router";
+import Navbar from "./components/navigation/Navbar";
 function App() {
+  const links = [
+    { id: 1, link: "Home", urlExt: "/" },
+    { id: 2, link: "About", urlExt: "/blah" },
+  ];
   return (
-    <BrowserRouter>
-      <div className="App">
-        <header className="App-header"></header>
-        <Router />
-      </div>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar links={links} />
+          <header className="App-header"></header>
+          <Router />
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 

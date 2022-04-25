239cae7c2a0315e91a421d10a21cc6c1c2da1f57
Umang A <umanga.eng@gmail.com>
2022-04-25 11:42:35 -0400

    feat: modified signup page, added login page, router updated to accomodate the new pages.

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -3,6 +3,8 @@ import Home from "../pages/Home.js";
 import About from "../pages/About";
 import Error404 from "../pages/errors/Error404.js";
 import BasicLayout from "../components/layouts/BasicLayout.js";
+import Signup from "../pages/login/Signup";
+import Login from "../pages/login/Login";
 export default function Router() {
   let element = useRoutes([
     {
@@ -16,6 +18,14 @@ export default function Router() {
         {
           path: "/about",
           element: <About />,
+        },
+        {
+          path: "/signup",
+          element: <Signup />,
+        },
+        {
+          path: "/login",
+          element: <Login />,
         },
         {
           path: "*",
